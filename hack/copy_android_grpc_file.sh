a48c48aa826d28585f2dfea19ab0d42211113064
KokiMatsumura <matsumura.koki1@gmail.com>
2019-09-19 19:52:47 +0900

    :sparkles: androidのプロジェクトにコピーできるようにした

--- hack/copy_android_grpc_file.sh
+++ hack/copy_android_grpc_file.sh
@@ -2,6 +2,6 @@
 
 cp -r ./bazel-bin/api/android_grpc/android_grpc.jar .
 jar -xvf ./android_grpc.jar 
-cp -r ./bolg_developers/sumimasen/api android/app/src/main/java/bolg_developers/sumimasen
+cp -r ./bolg_developers android/bolg/app/src/main/java
 rm -rf android_grpc.jar 
 rm -rf bolg_developers
